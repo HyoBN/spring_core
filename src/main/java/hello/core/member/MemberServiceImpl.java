a41e61a4dfebc018a866947b5dc314b7fad88090
Hyobin Yang <qls0786@khu.ac.kr>
2022-06-21 17:54:55 +0900

    주문과 할인 도메인 개발 및 테스트 코드 작성

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -5,8 +5,8 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
-    public void join(Member memer) {
-        memberRepository.save(memer);
+    public void join(Member member) {
+        memberRepository.save(member);
 
     }
 
